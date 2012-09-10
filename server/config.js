a066bcd3dd32687a769f8c84d1cf05b46b027764
Paul Winkler <paul.winkler@grapplemobile.com>
2012-09-10 14:26:33 +0100

    use /dev/cu not /dev/tty

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	port: 8081,
 	secret: 'er989c8ec£$Rev94wvsdf4lkw3',
-	comPort: /^tty.PL2303-/,
+	comPort: /^cu.PL2303-/,
 	database: __dirname+'/cards.sqlite',
 	users: {
 		'paul': 'password'

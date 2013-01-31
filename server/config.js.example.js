95d1280f939d4cf760c166d2180f821ff29fabfd
Paul Winkler <paul.winkler@grapplemobile.com>
2013-01-31 21:19:27 +0000

    distribute config template and ignore local config

--- server/config.js.example.js
+++ server/config.js.example.js
@@ -1,7 +1,7 @@
 module.exports = {
 	port: 8081,
 	secret: 'er989c8ec£$Rev94wvsdf4lkw3',
-	comPort: /^cu.PL2303-/,
+	comPort: /^ttyUSB0$/,
 	database: __dirname+'/cards.sqlite',
 	users: {
 		'paul': 'password'

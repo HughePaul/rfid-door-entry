d68077adc87da95c73827352994121107f00a3fd
Paul Winkler <paul.winkler@grapplemobile.com>
2014-02-12 14:32:09 +0000

    improve styling of time pattern selector

--- server/html/index.js
+++ server/html/index.js
@@ -1,5 +1,4 @@
-
-window.onload = function(){
+window.onload = function() {
 
 	var login = document.getElementById('login');
 	var username = document.getElementById('username');
@@ -22,10 +21,42 @@ window.onload = function(){
 	var cardNotes = document.getElementById('cardnotes');
 	var saveBtn = document.getElementById('saveBtn');
 
+	// checkbox handler
+	var patternHolder = document.getElementById('cardPattern');
+	var checkboxMouse = false;
+	var checkboxValue = false;
+	var checkboxDown = function(event) {
+		if (event.target.tagName === 'INPUT') {
+			checkboxValue = !this.checked;
+			this.checked = checkboxValue;
+		} else {
+			checkboxValue = true;
+		}
+		checkboxMouse = true;
+		event.preventDefault();
+	};
+	var checkboxOver = function(event) {
+		if (checkboxMouse) {
+			this.checked = checkboxValue;
+		}
+	};
+	var checkboxUp = function(event) {
+		checkboxMouse = false;
+		event.preventDefault();
+	};
+	var checkboxCancel = function(event) {
+		checkboxMouse = false;
+	};
 	var pattern = [];
-	for(var i=0; i<48; i++) {
-		pattern[i] = document.getElementById('cardpattern'+i);
+	for (var i = 0; i < 48; i++) {
+		pattern[i] = document.getElementById('cardPattern_' + i);
+		pattern[i].addEventListener('mousedown', checkboxDown);
+		pattern[i].addEventListener('mouseover', checkboxOver);
+		pattern[i].addEventListener('click', checkboxUp);
 	}
+	patternHolder.addEventListener('mousedown', checkboxDown);
+	document.body.addEventListener('mouseup', checkboxCancel);
+
 
 	saveBtn.disabled = true;
 	removeBtn.disabled = true;
@@ -38,13 +69,13 @@ window.onload = function(){
 
 	var cardCache = {};
 
-	socket.on('connect', function () {
+	socket.on('connect', function() {
 		var username = sessionStorage.getItem("username");
 		var cookie = sessionStorage.getItem("cookie");
 		socket.emit('auth', username, cookie);
 	});
 
-	socket.on('auth', function (username, cookie) {
+	socket.on('auth', function(username, cookie) {
 		sessionStorage.setItem("username", username);
 		sessionStorage.setItem("cookie", cookie);
 		addBtn.disabled = false;
@@ -53,34 +84,34 @@ window.onload = function(){
 		hideLogin();
 	});
 
-	socket.on('noauth', function () {
+	socket.on('noauth', function() {
 		sessionStorage.setItem("username", '');
 		sessionStorage.setItem("cookie", '');
 		reset();
 		showLogin();
 	});
 
-	socket.on('error', function (err) {
-		console.error('API error: ',err);
+	socket.on('error', function(err) {
+		console.error('API error: ', err);
 	});
 
-	socket.on('cards', function (cards) {
+	socket.on('cards', function(cards) {
 		updateCards(cards);
 	});
 
-	socket.on('card', function (id, card) {
+	socket.on('card', function(id, card) {
 		updateCard(id, card);
 	});
 
-	socket.on('logs', function (items) {
+	socket.on('logs', function(items) {
 		appendLogs(items);
 	});
 
-	socket.on('log', function (item) {
+	socket.on('log', function(item) {
 		appendLog(item);
 	});
 
-	socket.on('level', function (level) {
+	socket.on('level', function(level) {
 		currentLevel.value = level;
 	});
 
@@ -100,7 +131,7 @@ window.onload = function(){
 		login.style.display = 'none';
 		cardCache = cards;
 		cardsDiv.innerHTML = '';
-		for(var id in cards) {
+		for (var id in cards) {
 			updateCard(id, cards[id]);
 		}
 		addBtn.disabled = false;
@@ -108,31 +139,31 @@ window.onload = function(){
 
 	function updateCard(id, card) {
 		// update cache
-		if(!card) {
+		if (!card) {
 			delete cardCache[id];
 		} else {
 			cardCache[id] = card;
 		}
 
 		// update details display if currently shown
-		if(id === currentCardId) {
+		if (id === currentCardId) {
 			updateDetails(id);
 		}
 
 		// update card list
-		var cardDiv = document.getElementById('card-'+id);
+		var cardDiv = document.getElementById('card-' + id);
 		// if no details then remove the item
-		if(!card) {
-			if(cardDiv) {
+		if (!card) {
+			if (cardDiv) {
 				cardDiv.parentNode.removeChild(cardDiv);
 			}
 			return;
 		}
 
 		// if item doesn't exist then added it, otherwise blank it
-		if(!cardDiv) {
+		if (!cardDiv) {
 			cardDiv = document.createElement('div');
-			cardDiv.id = 'card-'+id;
+			cardDiv.id = 'card-' + id;
 			cardDiv.className = 'card';
 			cardDiv.onclick = function() {
 				updateDetails(id);
@@ -144,7 +175,7 @@ window.onload = function(){
 		// update item details
 		var avatar = document.createElement('div');
 		avatar.className = 'avatar';
-		avatar.style.backgroundImage = 'url('+ (card.avatar ? card.avatar : 'img/user.png') +')';
+		avatar.style.backgroundImage = 'url(' + (card.avatar ? card.avatar : 'img/user.png') + ')';
 		cardDiv.appendChild(avatar);
 		var name = document.createElement('div');
 		name.className = 'cardsName';
@@ -156,7 +187,7 @@ window.onload = function(){
 		cardDiv.appendChild(detail);
 		detail = document.createElement('div');
 		detail.className = 'cardsDetail';
-		detail.textContent = 'Level '+card.level;
+		detail.textContent = 'Level ' + card.level;
 		cardDiv.appendChild(detail);
 
 		var clear = document.createElement('div');
@@ -165,7 +196,9 @@ window.onload = function(){
 	}
 
 	function showLogin() {
-		if(authing) { return; }
+		if (authing) {
+			return;
+		}
 		authing = true;
 		console.log('Auth required');
 
@@ -181,9 +214,9 @@ window.onload = function(){
 		username.onkeypress = password.onkeypress = function(e) {
 			var code = (e.keyCode ? e.keyCode : e.which);
 			if (code === 13) {
-				if(!username.value){
+				if (!username.value) {
 					username.focus();
-				} else if(!password.value){
+				} else if (!password.value) {
 					password.focus();
 				} else {
 					doLogin();
@@ -192,7 +225,7 @@ window.onload = function(){
 			}
 		};
 
-		if(username.value) {
+		if (username.value) {
 			password.focus();
 		} else {
 			username.focus();
@@ -207,14 +240,14 @@ window.onload = function(){
 
 	function appendLogs(logs) {
 		logsDiv.innerHTML = '';
-		for(var i = 0; i < logs.length; i++) {
+		for (var i = 0; i < logs.length; i++) {
 			appendLog(logs[i]);
 		}
 	}
 
 	function appendLog(item) {
 		var log = document.createElement('div');
-		log.className = 'log '+item.type;
+		log.className = 'log ' + item.type;
 
 		var timestamp = document.createElement('span');
 		timestamp.className = 'timestamp';
@@ -233,28 +266,29 @@ window.onload = function(){
 
 		var card = cardCache[item.cardid];
 
-		if(item.cardid) {
+		if (item.cardid) {
 			log.onclick = function() {
 				updateDetails(item.cardid);
 			};
 		}
 
-		if(item.type === 'ADDED' || item.type === 'UPDATED' || item.type === 'REMOVED') {
+		if (item.type === 'ADDED' || item.type === 'UPDATED' || item.type === 'REMOVED') {
 			try {
 				var jsonCard = JSON.parse(item.desc);
-				if(jsonCard) { card = jsonCard; }
-			} catch(e) {
-			}
+				if (jsonCard) {
+					card = jsonCard;
+				}
+			} catch (e) {}
 		}
 
-		if(card) {
+		if (card) {
 			var logCard = document.createElement('div');
 			logCard.className = 'card';
 			logText.appendChild(logCard);
 
 			var avatar = document.createElement('div');
 			avatar.className = 'avatar';
-			avatar.style.backgroundImage = 'url('+ (card && card.avatar ? card.avatar : 'img/user.png') +')';
+			avatar.style.backgroundImage = 'url(' + (card && card.avatar ? card.avatar : 'img/user.png') + ')';
 			logCard.appendChild(avatar);
 
 			var logCardName = document.createElement('div');
@@ -264,23 +298,21 @@ window.onload = function(){
 			logCardId.textContent = item.cardid;
 			logCard.appendChild(logCardId);
 			var logCardLevel = document.createElement('div');
-			logCardLevel.textContent = 'Level '+item.level;
+			logCardLevel.textContent = 'Level ' + item.level;
 			logCard.appendChild(logCardLevel);
 
 			var cardClear = document.createElement('div');
 			cardClear.className = 'clear';
 			logCard.appendChild(cardClear);
-		}
-
-		else {
+		} else {
 			var logTextDesc = document.createElement('div');
 			logTextDesc.textContent = item.desc;
 			logText.appendChild(logTextDesc);
 
 			// log level
-	 		if(item.level) {
+			if (item.level) {
 				var logLevel = document.createElement('div');
-				logLevel.textContent = 'Level '+item.level;
+				logLevel.textContent = 'Level ' + item.level;
 				logText.appendChild(logLevel);
 			}
 
@@ -306,11 +338,11 @@ window.onload = function(){
 		var card = cardCache[id];
 		removeBtn.disabled = !card;
 
-		if(!card && !disable) {
+		if (!card && !disable) {
 			card = {};
 		}
 
-		cardAvatarImg.style.backgroundImage = 'url('+ (card && card.avatar ? card.avatar : 'img/user.png') +')';
+		cardAvatarImg.style.backgroundImage = 'url(' + (card && card.avatar ? card.avatar : 'img/user.png') + ')';
 		cardId.value = currentCardId;
 		cardName.value = card && card.name !== undefined ? card.name : '';
 		cardLevel.value = card && card.level !== undefined ? card.level : currentLevel.value;
@@ -319,31 +351,31 @@ window.onload = function(){
 
 		var now = new Date();
 		var hour = (now.getHours() * 2) + (now.getMinutes() >= 30 ? 1 : 0);
-		for(var i=0; i<48; i++) {
+		for (var i = 0; i < 48; i++) {
 			pattern[i].disabled = disable;
-			pattern[i].checked = (!card || card.pattern.substr(i,1) === '#');
+			pattern[i].checked = (!card || card.pattern.substr(i, 1) === '#');
 			pattern[i].className = (i === hour) ? 'currentHour' : '';
 		}
 
 		saveBtn.disabled =
-		cardId.disabled =
-		cardName.disabled =
-		cardLevel.disabled =
-		cardAvatar.disabled =
-		cardNotes.disabled = disable;
+			cardId.disabled =
+			cardName.disabled =
+			cardLevel.disabled =
+			cardAvatar.disabled =
+			cardNotes.disabled = disable;
 	}
 
 	cardAvatar.onchange = function() {
-		cardAvatarImg.style.backgroundImage = 'url('+ (cardAvatar.value ? cardAvatar.value : 'img/user.png') +')';
+		cardAvatarImg.style.backgroundImage = 'url(' + (cardAvatar.value ? cardAvatar.value : 'img/user.png') + ')';
 	};
 
 	saveBtn.onclick = function() {
-		if(!currentCardId) {
+		if (!currentCardId) {
 			currentCardId = cardId.value;
 		}
 
 		var timePattern = '';
-		for(var i=0; i<48; i++) {
+		for (var i = 0; i < 48; i++) {
 			timePattern += (pattern[i].checked) ? '#' : '-';
 		}
 
@@ -358,14 +390,14 @@ window.onload = function(){
 		// check details
 
 		// save
-		if(currentCardId) {
+		if (currentCardId) {
 			console.log('Save:', currentCardId, card);
 			socket.emit('card', currentCardId, card);
 		}
 	};
 
 	removeBtn.onclick = function() {
-		if(currentCardId && confirm('Are you sure you want to remove this card?')) {
+		if (currentCardId && confirm('Are you sure you want to remove this card?')) {
 			console.log('Remove:', currentCardId);
 			socket.emit('card', currentCardId);
 		}
@@ -377,7 +409,7 @@ window.onload = function(){
 	};
 
 	openBtn.onclick = function() {
-		if(confirm('Are you sure you want to open the door?')) {
+		if (confirm('Are you sure you want to open the door?')) {
 			console.log('Open');
 			socket.emit('open');
 		}
@@ -386,7 +418,7 @@ window.onload = function(){
 	currentLevel.onchange = function() {
 		var newLevel = currentLevel.value;
 		console.log('Change level:', newLevel);
-		socket.emit('level',newLevel);
+		socket.emit('level', newLevel);
 	};
 
 	logoutBtn.onclick = function() {
@@ -398,4 +430,3 @@ window.onload = function(){
 	reset();
 	showLogin();
 };
-

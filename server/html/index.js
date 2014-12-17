6e7e5dee25725adf740894e4ea2b74f3c20e66f0
Paul Winkler <hughe@hughe.co.uk>
2014-12-17 20:10:19 +0000

    specify resource to connect to

--- server/html/index.js
+++ server/html/index.js
@@ -75,7 +75,9 @@ window.onload = function() {
 	openBtn.disabled = true;
 
 	// connect socket and listen to events
-	var socket = io.connect(window.location.href);
+	var socket = io(window.location.href, {
+		'path': window.location.pathname + 'socket.io';
+	});
 
 	var cardCache = {};
 
